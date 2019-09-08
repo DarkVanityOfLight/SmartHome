a37a439e230637d9894bd765385637b2cedc841b
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2019-09-08 13:55:38 +0200

    Configuration

--- SmartHome/settings.py
+++ SmartHome/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
+    'Garage',
+    'Chatt',
+    'Home',
+    'Auth',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = 'Login'
+LOGIN_REDIRECT_URL = '/Home'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- SmartHome/urls.py
+++ SmartHome/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import path, include
 from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    path('', include('Garage.urls')),
+    path('', include('Chatt.urls')),
+    path('', include('Home.urls')),
+    path('', include('Auth.urls')),
 ]
