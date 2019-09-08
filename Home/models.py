e1a4ce73bf6fe4c5e4040d3b4fb302a2bfe4403b
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2019-09-08 14:07:20 +0200

    Database Setup and bug fixes

--- Home/models.py
+++ Home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class important_thing(models.Model):
+
+    Ersteller = models.CharField(max_length=30)
+    Datum = models.DateField(verbose_name="Datum")
+    body = models.TextField()
+
+    def __str__(self):
+        return self.bodys
--- Home/urls.py
+++ Home/urls.py
@@ -1,8 +1,8 @@
-from django.conf.urls import url, re_url
+from django.conf.urls import url, re_path
 from . import views
 
 urlpatterns = [
-    re_url(r'^$', views.home_View, name='home'),
+    re_path(r'^$', views.home_View, name='home'),
     url('Home', views.home_View),
     url('Pinnboard', views.create_pinned_View, name="Pinnboard"),
 ]
