c673d0112eab06e5bbe1f0567d2d025d035f8033
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-12 16:52:39 +0200

    feat: remove old structure

--- Garage/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-
-class GaragenForm(forms.Form):
-
-    SollStatus = forms.ChoiceField(choices=[('Auf','Auf'),('Zu','Zu')], widget=forms.Select(attrs={
-        'class': "my_form",
-    }))
-
--- Home/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, re_path
-from . import views
-
-urlpatterns = [
-    re_path(r'^$', views.home_View, name='home'),
-    url('Home', views.home_View),
-    url('Pinnboard', views.create_pinned_View, name="Pinnboard"),
-]
