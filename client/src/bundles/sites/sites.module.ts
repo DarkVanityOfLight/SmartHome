81e1c4bbc76550f86748d6baf262379924697cea
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-13 14:28:16 +0200

    feat: generate start component and hello world in app component

--- client/src/bundles/sites/sites.module.ts
+++ client/src/bundles/sites/sites.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainComponent } from './main/main.component';
+import { StartComponent } from './start/start.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [MainComponent, StartComponent],
   imports: [
     CommonModule
   ]
