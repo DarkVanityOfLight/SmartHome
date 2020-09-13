ca181243e01837f3ebfffddd80675030cf70035d
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-13 16:05:20 +0200

    feat: generate link-button component

--- client/src/bundles/commons/commons.module.ts
+++ client/src/bundles/commons/commons.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { LinkButtonComponent } from './link-button/link-button.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, LinkButtonComponent],
   imports: [
     CommonModule
   ],
---
+++ client/src/bundles/commons/link-button/link-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-link-button',
+  templateUrl: './link-button.component.html',
+  styleUrls: ['./link-button.component.styl']
+})
+export class LinkButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
