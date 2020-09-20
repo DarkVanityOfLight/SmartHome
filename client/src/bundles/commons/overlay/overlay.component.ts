af6c9adde2e4b444d3dba9802e38b20c1ae92d45
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-20 15:57:11 +0200

    feat: Subscribe to activate service

--- client/src/bundles/commons/overlay/overlay.component.ts
+++ client/src/bundles/commons/overlay/overlay.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivateService } from 'src/services/activate.service';
 
 @Component({
   selector: 'app-overlay',
@@ -6,12 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./overlay.component.styl']
 })
 export class OverlayComponent implements OnInit {
+  private isHidden: boolean;
 
-  constructor() { }
+  constructor(private activator: ActivateService) {
+    activator.close.subscribe(this.hide());
+    activator.open.subscribe(this.show());
+  }
 
   ngOnInit(): void {
     const burger = document.querySelector('.hamburger');
     const site = document.querySelector('.app');
+  }
+
+  show(): void {
+    this.isHidden = false;
+  }
+
+  hide(): void {
+    this.isHidden = true;
   }
 
 }
