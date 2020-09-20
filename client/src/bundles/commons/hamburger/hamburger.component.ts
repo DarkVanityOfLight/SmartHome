654c5bdec1c5a3ba891ff70f672c64a6a04df171
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-20 13:54:42 +0200

    feat: add a field to Hamburger that represents if the burger is clicked or not

--- client/src/bundles/commons/hamburger/hamburger.component.ts
+++ client/src/bundles/commons/hamburger/hamburger.component.ts
@@ -6,14 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hamburger.component.styl']
 })
 export class HamburgerComponent implements OnInit {
+  public clicked: boolean;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.clicked = false;
   }
 
   toggleHamburger(): void{
     const hamburger = document.querySelector('.hamburger');
     hamburger.classList.toggle('checked');
+    this.clicked = !this.clicked;
   }
 }
