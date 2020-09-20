e4ad89e6016f2b31b58484487330dc38c2f9331d
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-20 15:30:59 +0200

    TO SQUASH HEAD -1

--- client/src/bundles/commons/hamburger/hamburger.component.ts
+++ client/src/bundles/commons/hamburger/hamburger.component.ts
@@ -7,18 +7,15 @@ import { ActivateService } from 'src/services/activate.service';
   styleUrls: ['./hamburger.component.styl']
 })
 export class HamburgerComponent implements OnInit {
-  public clicked: boolean;
 
   constructor(private activatorService: ActivateService) { }
 
   ngOnInit(): void {
-    this.clicked = false;
   }
 
   toggleHamburger(): void{
     const hamburger = document.querySelector('.hamburger');
     hamburger.classList.toggle('checked');
-    this.clicked = !this.clicked;
     this.activatorService.toggle();
   }
 }
