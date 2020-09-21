0df4b4dcdbb1e58956f7f5fd7f49d11de6e58f64
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2020-09-21 17:39:47 +0200

    feat: register garage component at router and make all links point to it

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { StartComponent } from 'src/bundles/sites/start/start.component';
+import { GarageComponent } from 'src/bundles/sites/garage/garage.component';
 
 const routes: Routes = [
-  { path: '', component: StartComponent }
+  { path: '', component: StartComponent },
+  { path: 'Garage', component: GarageComponent }
 ];
 
 @NgModule({
